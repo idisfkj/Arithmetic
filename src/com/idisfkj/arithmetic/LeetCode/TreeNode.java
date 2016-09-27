43c6f98b8ce239fc0c513f8f5b258c79b821273c
idisfkj <idisfkj@qq.com>
2016-09-27 23:48:55 +0800

    LeetCode

---
+++ src/com/idisfkj/arithmetic/LeetCode/TreeNode.java
@@ -0,0 +1,14 @@
+package com.idisfkj.arithmetic.LeetCode;
+
+/**
+ * Created by idisfkj on 16/9/27.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
